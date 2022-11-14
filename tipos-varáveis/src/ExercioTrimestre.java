73d0e747dcd5f042667bc25de338109e7e5a3f20
PedroHQO <pedro39henrique.q.o@gmail.com>
2022-11-14 14:35:17 -0300

    Exercicio calculo trimestral com variaveis

---
+++ tipos-varáveis/src/ExercioTrimestre.java
@@ -0,0 +1,18 @@
+public class ExercioTrimestre {
+    //è preciso definir uma classe trimestral
+  public static void main(String[] args) {
+      //Fazendo o balanço do trimestre com os gastos em cada mês
+      int janeiro = 15000;
+      int fevereiro = 23000;
+      int março = 17000;
+      
+      //Realizando a soma trimestral
+      int somaTotal; 
+        somaTotal = janeiro + fevereiro + março;
+            System.out.println("Despesa total no trimestre");
+            System.out.println(somaTotal);
+  }
+
+
+}
+
---
+++ tipos-varáveis/src/Exercitando.java
@@ -0,0 +1,15 @@
+public class Exercitando {
+    public static void main(String[] args) {
+    //Declara idade
+        int idade;
+        idade = 15;
+        //Imprime idade
+        System.out.println(idade);
+
+        //Declara idadeNoAnoQueVem
+        int idadeNoAnoQueVem;
+            idadeNoAnoQueVem = idade + 1;
+        //Imprime idadeNoAnoQueVem
+        System.out.println(idadeNoAnoQueVem);
+    }
+}
