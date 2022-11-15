008a72249ff540f4f57e773db4239b48f4565b11
PedroHQO <pedro39henrique.q.o@gmail.com>
2022-11-15 18:01:22 -0300

    Gastos mensais

--- tipos-varáveis/src/ExercioTrimestre.java
+++ tipos-varáveis/src/ExercioTrimestre.java
@@ -4,11 +4,16 @@ public class ExercioTrimestre {
       //Fazendo o balanço do trimestre com os gastos em cada mês
       int janeiro = 15000;
       int fevereiro = 23000;
-      int março = 17000;
+      int marco = 17000;
       
+      
+        System.out.println("Despesa média mensal dos gastos");
+        System.out.println(janeiro);
+        System.out.println(fevereiro);
+        System.out.println(marco);
       //Realizando a soma trimestral
       int somaTotal; 
-        somaTotal = janeiro + fevereiro + março;
+        somaTotal = janeiro + fevereiro + marco;
             System.out.println("Despesa total no trimestre");
             System.out.println(somaTotal);
   }
