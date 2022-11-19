8e94d723ff5bc3320d6f8e9cd9908813de27eac3
PedroHQO <pedro39henrique.q.o@gmail.com>
2022-11-19 16:33:04 -0300

    Adicionando comentários

--- tipos-varáveis/src/ExercioTrimestre.java
+++ tipos-varáveis/src/ExercioTrimestre.java
@@ -6,17 +6,19 @@ public class ExercioTrimestre {
       int fevereiro = 23000;
       int marco = 17000;
       
-      
+      //Imrpiindo o nome e o valor gasto em cada mês
         System.out.println("Despesa média mensal dos gastos");
         System.out.println("JANEIRO $" + janeiro);
         System.out.println("FEVEREIRO $" + fevereiro);
         System.out.println("MARÇO $" + marco);
-      //Realizando a soma trimestral
+      
+        //Realizando a soma trimestral
       int somaTotal; 
         somaTotal = janeiro + fevereiro + marco;
             System.out.println("Despesa total no trimestre $" + somaTotal);
             
-    int mediaMensal;
+    //Realizando a média mensal gasta durante os 3 meses
+      int mediaMensal;
         mediaMensal = somaTotal / 3;
         System.out.println("A média mensal dos gastos são $" + mediaMensal);
   }
--- tipos-varáveis/src/Operadores.java
+++ tipos-varáveis/src/Operadores.java
@@ -1,7 +1,10 @@
 public class Operadores {
     
     public static void main(String[] args) {
-      
+        double d = 5; // ok, o double pode conter um número inteiro.
+int i = d; // não compila.
+
+        System.out.println(i);
     
     }
 }
